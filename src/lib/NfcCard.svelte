6a529501baf30a724e203150bcc4d3e91fa9be1e
Lilfa <lillyfandersen@gmail.com>
2026-02-02 12:10:59 +0100

    Added button

---
+++ src/lib/NfcCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { registerPlugin } from '@capacitor/core';
+
+  const CardData = registerPlugin<{
+    set(options: { hex: string }): Promise<void>;
+  }>('CardData');
+
+  function setCard() {
+    CardData.set({ hex: '414243' });
+  }
+</script>
+
+<button on:click={setCard}>
+  Set NFC Card Data
+</button>
